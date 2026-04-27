22ef63ee221199ea18db654b2d0175ab1e4aa815
chiara triveri <chiara.triveri@eng.it>
2026-04-27 08:37:00 +0000

    feat: Update settings and URLs for environment-specific configurations

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -26,10 +26,12 @@ SECRET_KEY = 'django-insecure-pqq-(&l5@m+#&!lt^c7l0z-$erak3_(dm8uvss+c_^l^m7*^c+
 DEBUG = True
 
 
+
 import os
-ALLOWED_HOSTS = ['*']
-if os.environ.get('CODESPACE_NAME'):
-    ALLOWED_HOSTS.append(f"{os.environ.get('CODESPACE_NAME')}-8000.app.github.dev")
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+codespace_name = os.environ.get('CODESPACE_NAME')
+if codespace_name:
+    ALLOWED_HOSTS.append(f"{codespace_name}-8000.app.github.dev")
 
 
 # Application definition
